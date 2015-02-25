4232da53d5a89d6c78e8e579592080b1816e93e6
AlexKVal <alexkval@gmail.com>
2015-02-25 13:24:08 +0200

    use Almond

--- build.js
+++ build.js
@@ -1,12 +1,21 @@
 /**
  * r.js -o build.js
  */
+
+/* jshint asi: true */
+/* jshint expr: true */
 ({
     baseUrl: ".",
     paths: {
       react: 'bower_components/react/react',
       'react-bootstrap': 'bower_components/react-bootstrap/react-bootstrap'
     },
-    name: "main",
-    out: "main-built.js"
+    out: "main-built.js",
+    findNestedDependencies: true,
+
+    name: 'bower_components/almond/almond',
+    include: "main",wrap: true,
+    // name: "main",
+    preserveLicenseComments: false,
+    // optimize: "none"
 })
